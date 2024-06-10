d61acb30f58095b5038751f3c43f44f07a7b015b
Nahaq789 <naha022677@icloud.com>
2024-06-10 22:55:35 +0000

    DI

--- src/User.API/Program.cs
+++ src/User.API/Program.cs
@@ -1,3 +1,5 @@
+using User.API.extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.StartupDI();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
