12ddeb01c74266c2f9f2691e9f946895460ea83a
Nahaq789 <naha022677@icloud.com>
2024-06-20 21:31:45 +0000

    maxlength変更

--- src/User.Infrastructure/Migrations/20240619223955_changePasswordMaxLength.Designer.cs
+++ src/User.Infrastructure/Migrations/20240619223955_changePasswordMaxLength.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using User.API.infrastructure;
@@ -11,9 +12,11 @@ using User.API.infrastructure;
 namespace User.Infrastructure.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20240619223955_changePasswordMaxLength")]
+    partial class changePasswordMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
