0abfcb134a30ed1846fe058c1cd0b80f55bb1ffa
Nahaq <naha022677@icloud.com>
2024-07-04 08:15:41 +0900

    Jwtトークンのテスト作成

--- test/User.API.Test/api/application/service/JwtTokenServiceTest.cs
+++ test/User.API.Test/api/application/service/JwtTokenServiceTest.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using User.API.application.service;
+using User.API.exceptions;
 using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
 namespace User.API.Test.application.service;
@@ -17,8 +18,8 @@ public class JwtTokenServiceTest
     public JwtTokenServiceTest()
     {
         this._configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true)
+            .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../../../User.API.Test"))
+            .AddJsonFile("appsettings.Test.json", optional: true, reloadOnChange: true)
             .Build();
         this._jwtTokenService = new JwtTokenService(_configuration);
     }
@@ -28,12 +29,130 @@ public class JwtTokenServiceTest
     {
         //arrange
         var token = new JwtSecurityTokenHandler().WriteToken(GenerateJwtToken(Email));
+        token = $"Bearer {token}";
 
         //act
         var result = _jwtTokenService.ValidateJwtToken(token);
 
         //assert
         Assert.True(result);
+    }
+
+    [Fact]
+    public void ValidateJwtToken_ReturnFalse()
+    {
+        //arrange
+        var token = new JwtSecurityTokenHandler().WriteToken(GenerateJwtToken(Email));
+        token = $"Bearer {token} test";
+
+        //act
+        var result = _jwtTokenService.ValidateJwtToken(token);
+
+        //assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ValidateJwtToken_ExpiredTokenReturnsFalse()
+    {
+        //arrange
+        //期限切れのトークンを作成
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Exp, DateTimeOffset.UtcNow.AddMinutes(30).ToUnixTimeSeconds().ToString())
+        };
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("Jwt:Jwt_Key") ?? string.Empty));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var expiredToken = new JwtSecurityToken(
+            issuer: _configuration.GetValue<string>("Jwt:Issuer"),
+            audience: _configuration.GetValue<string>("Jwt:Audience"),
+            claims: claims,
+            expires: DateTime.Now,
+            signingCredentials: credentials
+        );
+        var token = new JwtSecurityTokenHandler().WriteToken(expiredToken);
+        token = $"Bearer {token}";
+
+        //act
+        var result = _jwtTokenService.ValidateJwtToken(token);
+
+        //assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ValidateJwtToken_TokenWithInvalidIssuerReturnsFalse()
+    {
+        //arrange
+        //期限切れのトークンを作成
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Exp, DateTimeOffset.UtcNow.AddMinutes(30).ToUnixTimeSeconds().ToString())
+        };
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("Jwt:Jwt_Key") ?? string.Empty));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var expiredToken = new JwtSecurityToken(
+            issuer: "test",
+            audience: _configuration.GetValue<string>("Jwt:Audience"),
+            claims: claims,
+            expires: DateTime.Now,
+            signingCredentials: credentials
+        );
+        var token = new JwtSecurityTokenHandler().WriteToken(expiredToken);
+        token = $"Bearer {token}";
+
+        //act
+        var result = _jwtTokenService.ValidateJwtToken(token);
+
+        //assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ValidateJwtToken_TokenWithInvalidAudienceReturnsFalse()
+    {
+        //arrange
+        //期限切れのトークンを作成
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim(JwtRegisteredClaimNames.Exp, DateTimeOffset.UtcNow.AddMinutes(30).ToUnixTimeSeconds().ToString())
+        };
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("Jwt:Jwt_Key") ?? string.Empty));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var expiredToken = new JwtSecurityToken(
+            issuer: _configuration.GetValue<string>("Jwt:Issuer"),
+            audience: "test",
+            claims: claims,
+            expires: DateTime.Now,
+            signingCredentials: credentials
+        );
+        var token = new JwtSecurityTokenHandler().WriteToken(expiredToken);
+        token = $"Bearer {token}";
+
+        //act
+        var result = _jwtTokenService.ValidateJwtToken(token);
+
+        //assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ValidateJwtToken_TokenWithoutBearerPrefixIsValid()
+    {
+        //arrange
+        var token = new JwtSecurityTokenHandler().WriteToken(GenerateJwtToken(Email));
+
+        //act & assert
+        Assert.Throws<JwtTokenException>(() => _jwtTokenService.ValidateJwtToken(token));
     }
 
     private JwtSecurityToken GenerateJwtToken(string email)
